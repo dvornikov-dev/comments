9bd4c8e3890036eba03230f690da682da230c0e2
Aleksey Dvornikov <lesha.dvornikov1@gmail.com>
2023-02-13 19:09:51 +0200

    changed test data

---
+++ db/2.sql
@@ -0,0 +1,4 @@
+INSERT INTO "User" ("username", "email", "homeUrl") VALUES
+  ('admin', 'admin@example.com', 'http://example.com'),
+  ('user', 'user@example.com', 'http://example.com'),
+  ('marcus', 'marcus@example.com', 'http://example.com');
