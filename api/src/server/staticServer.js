db5ed5043e2bf3fe7edfe95f5554437375f0a534
Aleksey Dvornikov <lesha.dvornikov1@gmail.com>
2023-02-14 20:32:45 +0200

    static server

---
+++ api/src/server/staticServer.js
@@ -0,0 +1,57 @@
+import * as fs from 'node:fs';
+import * as http from 'node:http';
+import * as path from 'node:path';
+
+export default function staticServer(PORT) {
+  const MIME_TYPES = {
+    default: 'application/octet-stream',
+    html: 'text/html; charset=UTF-8',
+    js: 'application/javascript; charset=UTF-8',
+    css: 'text/css',
+    png: 'image/png',
+    jpg: 'image/jpg',
+    gif: 'image/gif',
+    ico: 'image/x-icon',
+    svg: 'image/svg+xml',
+  };
+
+  const HEADERS = {
+    'X-XSS-Protection': '1; mode=block',
+    'X-Content-Type-Options': 'nosniff',
+    'Strict-Transport-Security': 'max-age=31536000; includeSubdomains; preload',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  };
+
+  const STATIC_PATH = path.join(process.cwd(), './static');
+
+  const toBool = [() => true, () => false];
+
+  const prepareFile = async (url) => {
+    const paths = [STATIC_PATH, url];
+    if (url.endsWith('/')) paths.push('index.html');
+    const filePath = path.join(...paths);
+    const pathTraversal = !filePath.startsWith(STATIC_PATH);
+    const exists = await fs.promises.access(filePath).then(...toBool);
+    const found = !pathTraversal && exists;
+    const streamPath = found ? filePath : STATIC_PATH + '/404.html';
+    const ext = path.extname(streamPath).substring(1).toLowerCase();
+    const stream = fs.createReadStream(streamPath);
+    return { found, ext, stream };
+  };
+
+  http
+    .createServer(async (req, res) => {
+      const file = await prepareFile(req.url);
+      const statusCode = file.found ? 200 : 404;
+      const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
+      HEADERS['Content-Type'] = mimeType;
+      res.writeHead(statusCode, HEADERS);
+      file.stream.pipe(res);
+      console.log(`${req.method} ${req.url} ${statusCode}`);
+    })
+    .listen(PORT);
+
+  console.log(`Static Server running at http://127.0.0.1:${PORT}/`);
+}
