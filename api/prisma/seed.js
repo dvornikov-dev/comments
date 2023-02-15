9d45ca2d336b2274bc827ed9a216fcde36e472ae
Aleksey Dvornikov <lesha.dvornikov1@gmail.com>
2023-02-15 13:46:36 +0200

    + seeder

---
+++ api/prisma/seed.js
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const message = "{\"blocks\":[{\"key\":\"637gr\",\"text\":\"PRIVET<script></script>\",\"type\":\"unstyled\",\"depth\":0,\"inlineStyleRanges\":[],\"entityRanges\":[],\"data\":{}}],\"entityMap\":{}}";
+
+const array = [    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""},    {"message": ""}];
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'Test@prisma.io',
+      name: 'Test',
+      homeUrl: 'https://google.com',
+      comments: {
+        create: array
+      },
+    },
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
