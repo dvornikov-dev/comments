bdf356f557b9bf5a28cd7bb1b53d05e5d360876c
Aleksey Dvornikov <lesha.dvornikov1@gmail.com>
2023-02-15 16:11:10 +0200

    + seeder

---
+++ api/prisma/seed.js
@@ -0,0 +1,63 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const message =
+  // eslint-disable-next-line max-len
+  '{"blocks":[{"key":"637gr","text":"PRIVET<script></script>","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}';
+
+const array = [
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+  { message },
+];
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'Test@prisma.io',
+      username: 'Test',
+      homeUrl: 'https://google.com',
+      comments: {
+        create: array,
+      },
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- db/2.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "User" ("username", "email", "homeUrl") VALUES
-  ('admin', 'admin@example.com', 'http://example.com'),
-  ('user', 'user@example.com', 'http://example.com'),
-  ('marcus', 'marcus@example.com', 'http://example.com');
